2cf102caaadea186a71f794980de81ecc202070c
Luigi-Jala <luigi.cabrera@jala.university>
2026-06-04 16:16:02 -0500

    ✨ Add CryptoPrices component - First Decimal Pipe Example

--- angular-pipes/src/app/app.ts
+++ angular-pipes/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CryptoPrices } from './CryptoPrices/CryptoPrices';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CryptoPrices],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
