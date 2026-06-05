02447a4157f5d83815b503205c56b9f30fe6b14a
Luigi-Jala <luigi.cabrera@jala.university>
2026-06-05 14:25:09 -0500

    ✨ Add Locale config to Crypto Decimal Pipe Example

--- angular-project/src/app/Pipes/CryptoPrices/CryptoPrices.ts
+++ angular-project/src/app/Pipes/CryptoPrices/CryptoPrices.ts
@@ -1,6 +1,13 @@
 import { DecimalPipe } from "@angular/common";
 import { Component } from "@angular/core";
 
+// 1. IMPORTA EL REGISTRADOR Y LOS DATOS DE FRANCIA
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+
+// 2. REGISTRA EL IDIOMA INMEDIATAMENTE
+registerLocaleData(localeFr, 'fr');
+
 @Component({
     selector: 'crypto-prices',
     imports: [DecimalPipe],
